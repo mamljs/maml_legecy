06d73e998d17f45b3ab5356209a1f9496e81889f
Tyler Long <tyler4long@gmail.com>
2016-01-02 22:07:55 +0800

    Add bootstrap and create page header

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var program = require('commander');
 var path = require('path');
 var fs = require('fs');
 var yaml = require('js-yaml');
+var mustache = require('mustache');
+var ncp = require('ncp').ncp;
 
 
 program.version('0.0.1')
@@ -10,13 +12,23 @@ program.version('0.0.1')
     .parse(process.argv);
 
 
+ncp('node_modules/markdown-core/dist', path.join(program.output, 'dist'), function(err){
+    fs.unlink(path.join(program.output, 'dist/markdown-core.min.js'));
+});
+
+
 var config_file = path.join(program.input, 'config.yaml');
 var config = yaml.safeLoad(fs.readFileSync(config_file, 'utf8'));
 console.log(config);
 
+var templates_path = path.join(program.input, 'templates');
+var layout = fs.readFileSync(path.join(templates_path, 'layout.html'), 'utf8');
+console.log(layout);
 
 var home_page = path.join(program.input, 'index.md');
 var mdc = require('markdown-core/markdown-core-node');
 var result = mdc.render(fs.readFileSync(home_page, 'utf8'));
+console.log(result);
+result = mustache.render(layout, { content: result, title: config.name, brand: config.name });
 console.log(result);
 fs.writeFileSync(path.join(program.output, 'index.html'), result);
