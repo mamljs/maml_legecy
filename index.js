2f1b1ede529ee6debf1689c9eae9d237c434df29
Tyler Long <tyler4long@gmail.com>
2016-01-03 22:29:48 +0800

    Refactor code

--- index.js
+++ index.js
@@ -4,36 +4,47 @@ var fs = require('fs');
 var yaml = require('js-yaml');
 var mustache = require('mustache');
 var ncp = require('ncp').ncp;
+var mdc = require('markdown-core/markdown-core-node');
 
 
+// read and parse command line args
 program.version('0.0.1')
     .option('-i, --input [path]', 'Path to the input directory')
     .option('-o, --output [path]', 'Path to the output directory')
     .parse(process.argv);
 
 
+// copy assets into output directory
 ncp('node_modules/markdown-core/dist', path.join(program.output, 'dist'), function(err){
     fs.unlink(path.join(program.output, 'dist/markdown-core.min.js'));
 });
 
 
-var config_file = path.join(program.input, 'index.yaml');
-var config = yaml.safeLoad(fs.readFileSync(config_file, 'utf8'));
-console.log(config);
+function read_file(relative_path) {
+    var absolute_path = path.join(program.input, relative_path);
+    return fs.readFileSync(absolute_path, 'utf8');
+}
+var layout = read_file('templates/layout.html');
 
-var templates_path = path.join(program.input, 'templates');
-var layout = fs.readFileSync(path.join(templates_path, 'layout.html'), 'utf8');
-console.log(layout);
 
-var home_page = path.join(program.input, 'index.md');
-var mdc = require('markdown-core/markdown-core-node');
-var result = mdc.render(fs.readFileSync(home_page, 'utf8'));
-console.log(result);
-result = mustache.render(layout, {
-    content: result,
-    title: config.title + config.title_suffix,
-    brand: config.name,
-    navbar: '<li><a href="#">Link 1</a></li><li><a href="#">Link 2</a></li>'
-});
-console.log(result);
-fs.writeFileSync(path.join(program.output, 'index.html'), result);
+function write_file(relative_path, content) {
+    var absolute_path = path.join(program.output, relative_path);
+    fs.writeFileSync(absolute_path, content);
+}
+
+
+function generate_home_page() {
+    var config = yaml.safeLoad(read_file('index.yaml'));
+    var markdown = read_file('index.md');
+    var html = mdc.render(markdown);
+    html = mustache.render(layout, {
+        content: html,
+        title: config.title + config.title_suffix,
+        brand: config.brand,
+        navbar: '<li><a href="#">Link 1</a></li><li><a href="#">Link 2</a></li>'
+    });
+    console.log(html);
+    write_file('index.html', html);
+}
+
+generate_home_page();
--- sample/index.md
+++ sample/index.md
@@ -1,16 +1,17 @@
 # `website.md`
 
-`website.md` is a static website generator. User writes in markdown + yaml, `website.md` generates html.
+`website.md` is a static website generator written in node.js. User writes in markdown + yaml, `website.md` generates html.
 
 
 ## notes
 
-1. `*.md` contains page content.
-1. `*.yaml` contains configuration.
-1. for every directory, there is a `index.md`.
-1. for every `*.md`, there is a `*.yaml` with the same name.
-1. `*.yaml` inherits `index.yaml` in the same directory.
+1. Every web page is a directory, but not vice versa.
+1. Every directory contains `index.md` and `index.yaml`.
+1. `index.md` is page content, `index.yaml` is page configuration.
 1. `index.yaml` inherites `index.yaml` in parent directory.
+1. There is n special directories:
+    - `templates/` contains page templates.
+       - templates are based on [mustache](https://github.com/janl/mustache.js).
 
 
 ## usage
