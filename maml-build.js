3cbe92ad7f8b72a12ef6161dca5a4292b35d3a7f
Tyler Long <tyler4long@gmail.com>
2016-01-07 22:38:10 +0800

    maml build sub-command

---
+++ maml-build.js
@@ -0,0 +1,35 @@
+var nunjucks = require('nunjucks');
+var mdc = require('markdown-core/markdown-core-node');
+var configuration = require('./configuration');
+var file = require('./file');
+
+
+nunjucks.configure('templates', { autoescape: false });
+
+
+// clear the old output and re-copy assets
+file.reset();
+
+
+function generate_html(link) {
+    var config = configuration.get(link);
+    var markdown = file.read(link, 'index.md');
+    var html = mdc.render(markdown);
+    html = nunjucks.render('layout.html', {
+        content: html,
+        title: config.title + config.title_suffix,
+        brand: config.brand,
+        navbar: config.menu.reduce((result, _link) => {
+                if(link == _link) { // todo: change to starts with
+                    return result +  '<li class="active"><a href="' + _link + '">' + configuration.get(_link).name + '</a></li>';
+                } else {
+                    return result +  '<li><a href="' + _link + '">' + configuration.get(_link).name + '</a></li>';
+                }
+            }, '')
+    });
+    file.write(link, 'index.html', html);
+}
+
+
+// generate html for every index.md
+file.list().forEach(page => generate_html(page));
