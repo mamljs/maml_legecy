dbfd6198d47438bc267b8e39086738d2e545c9b5
Tyler Long <tyler4long@gmail.com>
2016-01-15 18:23:33 +0800

    Improve maml-build process

--- maml-build.js
+++ maml-build.js
@@ -21,12 +21,12 @@ function generate_html(link) {
     var config = configuration.get(link);
     var markdown = file.read(link, 'index.md');
     var html = mdc.render(markdown);
-    html = nunjucks.render('layout.html', { // todo: read layout config
+    html = nunjucks.render(config['layout'], {
         content: html,
         title: config.title + config.title_suffix,
         brand: config.brand,
         navbar: config.menu.reduce((result, _link) => {
-                if(link == _link) { // todo: change to starts with
+                if(link.startsWith(_link)) {
                     return result +  '<li class="active"><a href="' + _link + '">' + configuration.get(_link).name + '</a></li>';
                 } else {
                     return result +  '<li><a href="' + _link + '">' + configuration.get(_link).name + '</a></li>';
