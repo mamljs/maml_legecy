b96f3632232d7a4205b6a019f9b5c50dd5c31f8e
Tyler Long <tyler4long@gmail.com>
2016-01-15 18:14:29 +0800

    Read template name from argv

--- maml-init.js
+++ maml-init.js
@@ -1,8 +1,15 @@
+var program = require('commander');
 var path = require('path');
 var exec = require('child_process').exec;
 
 
-var template = 'default'; // todo: read template name from argv
+program
+  .version('1.0.0')
+  .option('-t, --template <template>', 'website template')
+  .parse(process.argv);
+
+
+var template = program.template || 'default';
 
 
 var current_path = path.resolve('.'); // current directory
@@ -10,7 +17,11 @@ exec(`
   cp ${__dirname}/templates/maml.yml ${current_path}/
   cp -r ${__dirname}/templates/${template}/layout ${current_path}/
   cp -r ${__dirname}/templates/${template}/content ${current_path}/
-`);
+`, (err) => {
+  if(err != null) {
+    console.log(err);
+  }
+});
 
 
 console.log(`Website initialized at ${current_path}`);
--- maml.js
+++ maml.js
@@ -2,7 +2,7 @@ var program = require('commander');
 
 
 program
-  .version('0.0.1')
+  .version('1.0.0')
   .command('init', 'initialize a new website')
   .command('build', 'build the website', { isDefault: true })
   .parse(process.argv);
