2acb40e5fd35054ba9efda6fd89191ae3e43eb36
Tyler Long <tyler4long@gmail.com>
2016-01-17 23:24:40 +0800

    Rewrite maml init

--- maml-init.js
+++ maml-init.js
@@ -12,12 +12,32 @@ program
 var template = program.template || 'default';
 
 
-// copy initial content to the new website
-exec(`cp -r ${__dirname}/templates/${template}/* .`, (err) => {
-  if(err != null) {
+// copy template content into current folder and run `npm install`
+exec(`wget https://github.com/mamljs/maml-template-${template}/archive/master.zip && unzip master.zip`, (err) => {
+  if(err) {
     console.log(err);
+    return;
   }
+  exec(`cp -r maml-template-${template}-master/assets .
+        cp -r maml-template-${template}-master/controllers .
+        cp -r maml-template-${template}-master/models .
+        cp -r maml-template-${template}-master/views .
+        cp -r maml-template-${template}-master/package.json .
+  `, (err) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    exec(`rm -rf master.zip && rm -rf maml-template-${template}-master`);
+    exec(`npm install`, (err) => {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      console.log('Website initialized');
+    });
+  });
 });
 
 
-console.log('Website initialized');
+console.log(`Website initializing with template '${template}'`);
