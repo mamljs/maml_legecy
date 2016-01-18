d7611ca67ee02c49bac5ae3a1c5615a1ebf911d6
Tyler Long <tyler4long@gmail.com>
2016-01-18 10:56:24 +0800

    Refactor code

--- maml-build.js
+++ maml-build.js
@@ -1,4 +1,3 @@
-var R = require('ramda');
 var program = require('commander');
 var nunjucks = require('nunjucks');
 var path = require('path');
@@ -31,33 +30,29 @@ file.clean();
 file.copyAssets();
 
 
-// generate html for every index.md
-file.list().forEach(pathname => dispatch(pathname));
-
-
 function Page(pathname) {
   this.pathname = pathname;
   this.markdown = file.read(pathname, 'index.md');
-  const config = g.config[pathname];
+  var config = g.config[pathname];
   for (var attr in config) {
     this[attr] = config[attr];
   }
   this.g = g;
   this.html = 'hello world';
   this.generate = function() {
-    const html = nunjucks.render(`${this.view}.html`, this);
-    file.write(this.pathname, 'index.html', html);
+    var html = nunjucks.render(`${this.view}.html`, this);
+    file.write(pathname, 'index.html', html);
   }
 }
 
 
-// process a pathname
-function dispatch(pathname) {
+// generate html for every index.md
+file.list().forEach(pathname => {
   var page = new Page(pathname);
-  const controller = require(path.resolve(`./controllers/${page.controller}`));
-  const action = controller[page.action];
+  var controller = require(path.resolve(`./controllers/${page.controller}`));
+  var action = controller[page.action];
   action(page);
-}
+});
 
 
 console.log('Website built');
